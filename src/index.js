35228ebb77a131e9ee97e654bffc528070801121
shyaminid <shyamini0306@gmail.com>
2022-02-06 22:14:39 +0530

    strictmode changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
- <StrictMode>
+  <React.StrictMode>
     <Router>
-    <App />
-    </Router>,
-  document.getElementById('root')
-  </StrictMode>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
